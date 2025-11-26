2008f123ea29020edeb51f4ea1575ce725120e03
vegabook <thomas@scendance.fr>
2025-11-26 00:39:27 +0000

    refactor

--- hdf5_convert.py
+++ hdf5_convert.py
@@ -1,29 +1,41 @@
-# ---------- convert ginormous hdf5 to memory mappable parquet using polars ------------
+# ---------- convert large hdf5 to memory mappable parquet using pyarrow  ------------
+# colorscheme lackluster-night
 
 import h5py
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pyarrow.dataset as ds # for proper chunking
 import numpy as np
 from pathlib import Path
 from loguru import logger
 
-Path("data/parquet").mkdir(exist_ok=True)
+infile = Path("./data/hdf5/ES.h5")
+setname = "trades" # "trades_filter0vol" or "trades"
+outdir = Path("./data/rawinput") / setname
+outdir.mkdir(exist_ok = True, parents = True)
 
-with h5py.File("./data/hdf5/ES.h5", "r") as f:
-    dset = f["tick"]["trades_filter0vol"]
-    n_rows = dset.shape[0]
-    chunk = 50_000_000
-    print(f"total rows are {n_rows}, total chunks are {n_rows / chunk}")
+def convert():
 
-    for i, start in enumerate(range(0, n_rows, chunk)):
-        data = dset[start:start + chunk]
-        
-        table = pa.Table.from_arrays([
-            pa.array(data["Instrument"]),
-            pa.array(data["Price"]),
-            pa.array(data["Time"].astype('U17')),   # string
-            pa.array(data["Volume"])
-        ], names=["Instrument", "Price", "Time", "Volume"])
-        
-        pq.write_table(table, f"data/parquet/es_{i:03d}.parquet")
-        print(f"Wrote part {i} – rows {start}–{start+len(data)}")
+    with h5py.File("./data/hdf5/ES.h5", "r") as f:
+        dset = f["tick"][setname]
+        n_rows = dset.shape[0]
+        chunk = 10_000_000
+        print(f"total rows are {n_rows}, total chunks are {n_rows / chunk}")
+
+        for iout, start in enumerate(range(0, n_rows, chunk)):
+            data = dset[start:start + chunk]
+            
+            table = pa.Table.from_arrays([
+                pa.array(data["Instrument"]),
+                pa.array(data["Price"]),
+                pa.array(data["Time"].astype('U17')),   # string
+                pa.array(data["Volume"])
+            ], names=["Instrument", "Price", "Time", "Volume"])\
+            .sort_by([("Time", "ascending")])
+            
+            pq.write_table(table, outdir / f"es_{iout:04d}.parquet")
+            print(f"Wrote part {iout} – rows {start}–{start+len(data)}")
+
+
+if __name__ == "__main__":
+    convert()
--- main.py
+++ main.py
@@ -1,27 +1,146 @@
 # --------------------- ADIA Question 11 Tick Data -------------------------------
 # ------------- main parquet disk based lazy analysis routines -------------------
 # --------------------------------------------------------------------------------
-# colorscheme 
+# colorscheme lackluster-dark
 
 import polars as pl
 from loguru import logger
 from pathlib import Path
 from rich.console import Console; console = Console()
 from pprint import pprint
+import IPython
+IPython.embed
+import datetime as dt
 
-DATALOC = Path(__file__).resolve().parent / "data" / "parquet"
-console.print(f"[bright blue]Data location set to {DATALOC}")
+from hdf5_convert import setname, outdir as indir
+SETIN = indir / "*.parquet"
+DIROUT = Path(__file__).resolve().parent / "data" / f"processed_{setname}"
+DIROUT.mkdir(exist_ok = True, parents = True)
 
-def unique_tickers():
-    ccols = pl.scan_parquet(str(DATALOC))\
+
+def unique_tickers(setfl):
+    """ return unique tickers in the dataset """
+    ccols = pl.scan_parquet(stfl)\
         .select("Instrument")\
         .unique()\
         .collect()
     return ccols
+
+def columns(setfl):
+    """column names"""
+    return pl.read_parquet_schema(setfl)
+
+
+def trs(inp, outp):
+    """ Makes a total return series from all the prices in each contract.
+    Uses polars lazy scans and is therefore able to operate on data sets bigger than memory.
+    Algo: sort over instrument, time, group over instrument, take percentage returns
+          make first nulls zero, cum_prod (returns+1), multiply by first price 
+          in entire set.
+    inp: string input file(s) 
+    outp: string output file location
+    """
+    (
+        pl.scan_parquet(inp, low_memory = True)
+        .sort(["Instrument", "Time"])
+        .with_columns(
+            pl.col("Price")
+                .pct_change()
+                .over("Instrument")
+                .fill_null(0)
+                .alias("pct_change")
+        )
+        .sort(["Time"])
+        .with_columns(
+            (pl.col("pct_change") + 1)
+            .cum_prod()
+            .alias("pct_cumprod")
+        )
+        .with_columns(
+            (pl.col("pct_cumprod") * pl.col("Price").first()).alias("trs")
+        )
+        .sink_parquet(outp, compression = "zstd")
+    )
+
+
+def trs2(inp, outp):
+    """ Makes a total return series from all the prices in each contract.
+    Uses polars lazy scans and is therefore able to operate on data sets bigger than memory.
+    Algo: sort over instrument, time, group over instrument, take percentage returns
+          make first nulls zero, cum_prod (returns+1), multiply by first price 
+          in entire set.
+    inp: string input file(s) 
+    outp: string output file location
+    """
+    (
+        pl.scan_parquet(inp, low_memory = True)
+        .sort(["Time"])
+        .with_columns(
+            pl.col("Price")
+                .pct_change()
+                .over("Instrument")
+                .fill_null(0)
+                .alias("pct_change")
+        )
+        .sort(["Time"])
+        .with_columns(
+            (pl.col("pct_change") + 1)
+            .cum_prod()
+            .alias("pct_cumprod")
+        )
+        .with_columns(
+            (pl.col("pct_cumprod") * pl.col("Price").first()).alias("trs")
+        )
+        .sink_parquet(outp, compression = "zstd")
+    )
     
+
+def trs3(inp, outp):
+    """ Makes a total return series from all the prices in each contract.
+    Uses polars lazy scans and is therefore able to operate on data sets bigger than memory.
+    Algo: sort over instrument, time, group over instrument, take percentage returns
+          make first nulls zero, cum_prod (returns+1), multiply by first price 
+          in entire set.
+    inp: string input file(s) 
+    outp: string output file location
+    """
+    (
+        pl.scan_parquet(inp, low_memory = True)
+        .sort(["Time"])
+        .with_columns(
+            pl.col("Price")
+                .pct_change()
+                .over("Instrument")
+                .fill_null(0)
+                .alias("pct_change")
+        )
+        .with_columns(
+            (pl.col("pct_change") + 1)
+            .cum_prod()
+            .alias("pct_cumprod")
+        )
+        .with_columns(
+            (pl.col("pct_cumprod") * pl.col("Price").first()).alias("trs")
+        )
+        .sink_parquet(outp, compression = "zstd")
+    )
+
+
 if __name__ == "__main__":
-    ut = unique_tickers()
-    pprint(ut)
-    pprint(f"Count: {len(ut)}")
+    #ut = unique_tickers()
+    #pprint(ut)
+    #pprint(f"Count: {len(ut)}")
+    print(columns(SETIN))
+    nowtime = dt.datetime.now()
+    logger.info(f"time taken test: {(dt.datetime.now - nowtime).total_seconds()}")
+    nowtime = dt.datetime.now()
+    trs(SETIN, DIROUT / "trs.parquet")
+    logger.info(f"time taken trs: {(dt.datetime.now - nowtime).total_seconds()}")
+    nowtime = dt.datetime.now()
+    trs2(SETIN, DIROUT / "trs2.parquet")
+    logger.info(f"time taken trs2: {(dt.datetime.now - nowtime).total_seconds()}")
+    nowtime = dt.datetime.now()
+    trs3(SETIN, DIROUT / "trs3.parquet")
+    logger.info(f"time taken trs3: {(dt.datetime.now - nowtime).total_seconds()}")
     
 
